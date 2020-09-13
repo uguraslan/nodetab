2d653b4497c49fd4e1c0ed5d0efe6fbb37a01c60
Ugur Aslan <ugur@uguraslan.net>
2020-09-13 14:04:17 +0300

    cleaned unused terminal colors

--- cron.sh
+++ cron.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
+# the color code taken and modified from : https://deb.nodesource.com/setup_14.x
 if test -t 1; then # if terminal
     ncolors=$(which tput > /dev/null && tput colors) # supports color
     if test -n "$ncolors" && test $ncolors -ge 8; then
-        termcols=$(tput cols)
         bold="$(tput bold)"
-        underline="$(tput smul)"
-        standout="$(tput smso)"
         normal="$(tput sgr0)"
-        black="$(tput setaf 0)"
-        red="$(tput setaf 1)"
-        green="$(tput setaf 2)"
-        yellow="$(tput setaf 3)"
-        blue="$(tput setaf 4)"
-        magenta="$(tput setaf 5)"
         cyan="$(tput setaf 6)"
-        white="$(tput setaf 7)"
     fi
 fi
 
